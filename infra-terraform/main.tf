00ddc4bf6b91c6a1565b49a66bc260022c063f4a
Italo <italosp00@gmail.com>
2026-03-16 13:12:26 -0300

    fix(terraform): use k8s 1.32 for free tier support

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -89,7 +89,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aks-keycloak-poc"
-  kubernetes_version  = "1.29"
+  kubernetes_version  = "1.32"
+  sku_tier            = "Free"
 
   default_node_pool {
     name            = "systempool"
